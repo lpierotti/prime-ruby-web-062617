31dddc9594fcb2b44ee142c69d7ec97185287dab
Luke Pierotti <lpierotti2@gmail.com>
2017-06-08 19:55:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  end
+
+  array = (2..int-1).to_a
+
+  array.each do |num|
+    if int % num == 0
+      return false
+    end
+  end
+  true
+end
